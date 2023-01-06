f3b3165f38bb157135ba0ec922af853767db83c3
tschome <1054367842@qq.com>
2023-01-06 17:27:05 +0800

    更新main示例代码

--- main.c
+++ main.c
@@ -10,19 +10,19 @@ void main(int argc, int **argv)
 {
 	log_set_flags(LOG_SKIP_REPEATED | LOG_PRINT_LEVEL);//跳过重复的信息 + 显示打印级别
 
-	log_set_level(LOG_INFO);
+	log_set_level(LOG_MAX_OFFSET);
 	int i = 0;
-	for(i = 0; i < 10; i++)
-		log(TAG, LOG_INFO, "success 0\n");
-	for(i = 0; i < 10; i++)
-		log(TAG, LOG_TRACE, "success 1\n");
-	for(i = 0; i < 10; i++)
-		log(TAG, LOG_ERROR, "success 2\n");
-	for(i = 0; i < 10; i++)
-		log(TAG, LOG_DEBUG, "success 3\n");
-	for(i = 0; i < 10; i++)
-		log(TAG, LOG_WARNING, "success 4\n");
-		log(TAG, LOG_WARNING, "success 8\n");
+	//for(i = 0; i < 10; i++)
+	log(TAG, LOG_QUIET, "LOG_QUIET\n");
+	log(TAG, LOG_PANIC, "LOG_PANIC\n");
+	log(TAG, LOG_FATAL, "LOG_FATAL\n");
+	log(TAG, LOG_ERROR, "LOG_ERROR\n");
+	log(TAG, LOG_WARNING, "LOG_WARNING\n");
+	log(TAG, LOG_INFO, "LOG_INFO\n");
+	log(TAG, LOG_VERBOSE, "LOG_VERBOSE\n");
+	log(TAG, LOG_DEBUG, "LOG_DEBUG\n");
+	log(TAG, LOG_TRACE, "LOG_TRACE\n");
+	log(TAG, LOG_MAX_OFFSET, "LOG_MAX_OFFSET\n");
 	return;
 }
 
